d7d7f6b46eb8adefa27bcddafe3875167ab7f24b
Filippo Guerranti <l4plac3@gmail.com>
2020-12-29 10:44:21 +0100

    Update DSF.DisjointSetForest in GraphBasedSegmentation.py

--- segmentation/GraphBasedSegmentation.py
+++ segmentation/GraphBasedSegmentation.py
@@ -221,7 +221,7 @@ class GraphBasedSegmentation:
         Returns:
             components (DisjointSetForest): Disjoint-set Forest containing the segmented components
         """
-        self.components = DisjointSetForest(self.num_nodes)
+        self.components = DSF.DisjointSetForest(self.num_nodes)
         threshold = [GraphBasedSegmentation._threshold(k, i) for i in self.components.size]
 
         self.sort()
